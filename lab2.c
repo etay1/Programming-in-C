1abf0798ba774018f1bac47b00fe57ffa22bd5a5
Elijah Tay <98202550+etay1@users.noreply.github.com>
2023-01-26 17:52:00 -0500

    Create lab2.c

---
+++ lab2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+ 
+int main () {
+printf("Enter two digits");
+scanf("%i", n1);
+scanf("%i", n2);
+co(&n1, &n2);
+  return 0;
+}
+void co (int *x, int *y) {
+  
+  while (x>0 && y>0) {
+  r1 = x%10
+  r2 = y%10
+  }
+   return;
+}

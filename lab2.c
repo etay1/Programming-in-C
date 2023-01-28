46ba224136d48c2eb1923486179312014fe382f9
Elijah Tay <98202550+etay1@users.noreply.github.com>
2023-01-27 19:53:33 -0500

    Update lab2.c

--- lab2.c
+++ lab2.c
@@ -1,17 +1,26 @@
 #include <stdio.h>
- 
+
+int co(int a, int i);
+
 int main () {
-printf("Enter two digits");
-scanf("%i", n1);
-scanf("%i", n2);
-co(&n1, &n2);
-  return 0;
+    printf("Enter two digits\n");
+    int a,b,t;
+    scanf("%i %i", &a, &b);
+    t = co(a,b);
+    printf("%i", t);
+    return 0;
 }
-void co (int *x, int *y) {
-  
-  while (x>0 && y>0) {
-  r1 = x%10
-  r2 = y%10
-  }
-   return;
+
+
+int co(int x, int y) {
+    int c = 0;
+
+
+//
+//    while (x>0 && y>0) {
+//        r1 = x%10
+//        r2 = y%10
+//    }
+
+    return c;
 }

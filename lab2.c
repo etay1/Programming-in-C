5880581ed61ab64bc4ef6680dd5ebee5364b5ed0
Elijah Tay <98202550+etay1@users.noreply.github.com>
2023-02-05 18:26:11 -0500

    Update lab2.c

--- lab2.c
+++ lab2.c
@@ -1,26 +1,42 @@
 #include <stdio.h>
 
-int co(int a, int i);
+int main() {
+    int a,b,cc=0,ca=0,s=0,r;
 
-int main () {
-    printf("Enter two digits\n");
-    int a,b,t;
-    scanf("%i %i", &a, &b);
-    t = co(a,b);
-    printf("%i", t);
+    printf("Enter two numbers\n");
+    scanf("%d %d", &a, &b);
+
+    //if a = 0 exit
+    while(a!=0) {
+        r = a + b;
+        ca = 0;
+        cc = 0;
+        while(a!=0&&b!=0) {
+            s=0;
+            s+= a % 10; // last digit of a += s
+            s+= b % 10; // last digit of b += s
+            s+=ca;// add carry to the s
+            if(s > 9) {// if s greater than 9 means carry
+                cc++; // increment the ca
+                ca=1; // set ca to 1
+            }
+            else {// if the sum is less than 10, no carry
+                ca=0; //
+            }
+            a=a/10; // remove last digit frob a
+            b=b/10; // remove last digit from b
+        }
+
+        if(cc == 0)
+            printf("No carries are required");
+        else
+            printf("\n%d carries required", cc);
+
+        printf("\nThe addition of two numbers is %d", r);
+        printf("\nEnter two numbers\n");
+        scanf("%d %d",&a,&b);
+    }
+
     return 0;
-}
 
-
-int co(int x, int y) {
-    int c = 0;
-
-
-//
-//    while (x>0 && y>0) {
-//        r1 = x%10
-//        r2 = y%10
-//    }
-
-    return c;
 }

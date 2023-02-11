e9f39aa8fdc79a5c30ab27534523abfc299a6146
Elijah Tay <98202550+etay1@users.noreply.github.com>
2023-02-11 03:31:09 -0500

    Update lab4.c

--- lab4.c
+++ lab4.c
@@ -35,7 +35,9 @@ int main() {
     printf("Value of dubptr1 : %f \n",*dubptr1);
     printf("Value of dubptr2 : %f \n",*dubptr2);
 
-    printf("\n4.\tAssign intptr2 to intptr1 and record the value of intptr1. Assign intptr1 to dubptr1 and record what happens. Use typecasting to cast the type of intptr1 to type (double *) and assign this to dubptr1 and record the value in dubptr1.\n");
+    printf("\n4.\tAssign intptr2 to intptr1 and record the value of intptr1. \n"
+           "Assign intptr1 to dubptr1 and record what happens. \n"
+           "Use typecasting to cast the type of intptr1 to type (double *) and assign this to dubptr1 and record the value in dubptr1.\n");
     intptr1=intptr2;
     dubptr1=(double *)intptr1;
     printf("Value of intptr1: %d \n",*intptr1);
@@ -56,24 +58,24 @@ int main() {
     //  Process finished with exit code 139 (interrupted by signal 11: SIGSEGV)
 
     printf("\n7.\tAssign the value 100 to * intptr2 and record the value of both i1 and i2.");
-    printf("value of *intptr2 : <%d>\n",*intptr2);
+    printf("Value of *intptr2 : %d\n",*intptr2);
     *intptr2 = 100;
     printf("Value of i1 :  %d\n",i1);
     printf("Value of i2 :  %d\n",i2);
 
     printf("\n8.\tFor intptr2 and dubptr2, record the value of and the dereferenced value of the pointer + 1 and the pointer – 1.\n");
-    printf("(intptr2 + 1) : <%p> (dubptr2 + 1) : <%p>\n", (intptr2 + 1), (dubptr2 + 1));
-    printf("*(intptr2 + 1) : <%d> *(dubptr2 + 1) : <%lf>\n", *(intptr2 + 1), *(dubptr2 + 1));
-    printf("(intptr2 - 1) : <%p> (dubptr2 - 1) : <%p>\n", (intptr2 - 1), (dubptr2 - 1));
-    printf("*(intptr2 - 1) : <%d> *(dubptr2 - 1) : <%lf>\n", *(intptr2 - 1), *(dubptr2 - 1));
+    printf("(intptr2 + 1) : %p (dubptr2 + 1) : %p\n", (intptr2 + 1), (dubptr2 + 1));
+    printf("*(intptr2 + 1) : %d *(dubptr2 + 1) : %lf\n", *(intptr2 + 1), *(dubptr2 + 1));
+    printf("(intptr2 - 1) : %p (dubptr2 - 1) : %p\n", (intptr2 - 1), (dubptr2 - 1));
+    printf("*(intptr2 - 1) : %d *(dubptr2 - 1) : %l\n", *(intptr2 - 1), *(dubptr2 - 1));
 
     printf("\n9.\tSet intptr1 to the address of i1. Record the answers to the following questions:\n");
     intptr1 = &i1;
-    printf("( intptr1 == intptr2) : <%d>\n", (intptr1 == intptr2));
-    printf("(*intptr1 == *intptr2) : <%d>\n", (*intptr1 == *intptr2));
+    printf("( intptr1 == intptr2) : %d\n", (intptr1 == intptr2));
+    printf("(*intptr1 == *intptr2) : %d\n", (*intptr1 == *intptr2));
     intptr1 = &i2;
-    printf("(intptr1 == intptr2) : <%d>\n", (intptr1 == intptr2));
-    printf("(*intptr1 == *intptr2) : <%d>\n", (*intptr1 == *intptr2));
+    printf("(intptr1 == intptr2) : %d\n", (intptr1 == intptr2));
+    printf("(*intptr1 == *intptr2) : %d\n", (*intptr1 == *intptr2));
 
     printf("\n10.\tThe malloc function assigns new memory to a pointer variable.\n"
            "It returns type void * so you must typecast it to the correct type.\n"
@@ -87,10 +89,10 @@ int main() {
     printf("\n11.\tTo deallocate the dynamic memory of a pointer, we use the free () function.\n"
            "This function requires us to typecast our pointer to type void *.\n"
            "The syntax is: free ( (void *) ptr); Deallocate the memory for ptr and reallocate it again.\n"
-           "Is the value of ptr the same as it was in question 10? No it isn't\n");
+           "Is the value of ptr the same as it was in question 10? \n");
 
     free((void*)ptr);
-    ptr = (double*)malloc(sizeof(double));
+    ptr = (void*)malloc(sizeof(double));
     printf("Value of ptr post reallocation : %p \n",ptr);
 
     return 0;

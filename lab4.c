b94299d7ab312307c45ff06c7da1cf7f63f09266
Elijah Tay <98202550+etay1@users.noreply.github.com>
2023-02-11 03:52:44 -0500

    Update lab4.c

--- lab4.c
+++ lab4.c
@@ -2,11 +2,10 @@
 #include <stdlib.h>
 
 int main() {
-
+    
+    printf("1.\tRecord the addresses of the variables.\n");
     int i1, i2;
     double d1, d2;
-
-    printf("1.\tRecord the addresses of the variables.\n");
     printf("%p\n", (void *)&i1);
     printf("%p\n", (void *)&i2);
     printf("%p\n", (void *)&d1);
@@ -24,12 +23,10 @@ int main() {
 
     printf("\n3.\tAssign the address of i1 to intptr1, the address of d1 to dubptr1 and so on. "
            "Record the data values stored in the pointer variables.");
-
     intptr1=&i1;
     intptr2=&i2;
     dubptr1=&d1;
     dubptr2=&d2;
-
     printf("\nValue of intptr1 : %d \n",*intptr1);
     printf("Value of intptr2 : %d \n",*intptr2);
     printf("Value of dubptr1 : %f \n",*dubptr1);
@@ -43,7 +40,6 @@ int main() {
     printf("Value of intptr1: %d \n",*intptr1);
     printf("Value of dubptr1 after typecasting: %f \n",*dubptr1);
 
-
     printf("\n5.\tAssign the value NULL to intptr1 and record the value that is output for intptr1.\n");
     intptr1 = NULL;
     printf("Value of intptr1 : %d \n", intptr1);
@@ -51,7 +47,6 @@ int main() {
     printf("\n6.\tDereference the pointer intptr2\n");
     *intptr2=100;
     printf("Value of intptr2 : %d \n",*intptr2);
-
     //  Try to dereference the pointer intptr1 (which is set to NULL) and see what happens.
     //  If this causes a problem with the program, record the problem and remove the code.
     //  *intptr1=8;
@@ -67,7 +62,7 @@ int main() {
     printf("(intptr2 + 1) : %p (dubptr2 + 1) : %p\n", (intptr2 + 1), (dubptr2 + 1));
     printf("*(intptr2 + 1) : %d *(dubptr2 + 1) : %lf\n", *(intptr2 + 1), *(dubptr2 + 1));
     printf("(intptr2 - 1) : %p (dubptr2 - 1) : %p\n", (intptr2 - 1), (dubptr2 - 1));
-    printf("*(intptr2 - 1) : %d *(dubptr2 - 1) : %l\n", *(intptr2 - 1), *(dubptr2 - 1));
+    printf("*(intptr2 - 1) : %d *(dubptr2 - 1) : %lf\n", *(intptr2 - 1), *(dubptr2 - 1));
 
     printf("\n9.\tSet intptr1 to the address of i1. Record the answers to the following questions:\n");
     intptr1 = &i1;
@@ -90,11 +85,10 @@ int main() {
            "This function requires us to typecast our pointer to type void *.\n"
            "The syntax is: free ( (void *) ptr); Deallocate the memory for ptr and reallocate it again.\n"
            "Is the value of ptr the same as it was in question 10? \n");
-
     free((void*)ptr);
     ptr = (void*)malloc(sizeof(double));
     printf("Value of ptr post reallocation : %p \n",ptr);
     printf("Value of *ptr : %lf \n",*ptr);
-    
+
     return 0;
 }
